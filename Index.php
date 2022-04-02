ffe159fe8b2334f4dd6bbbe165df4f6f0732f35d
Hasnat <abuhasnat.hemel@gmail.com>
2022-04-02 11:57:26 +0600

    Day One index.php

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+   <!-- <title>PHP MYSQL Batch:10</title> -->
+   <title><?php echo "PHP MYSQL Batch:10" ?></title>
+</head>
+
+<body>
+    <?php echo "HI!" ?>
+    <h1>Web Devloping usin PHP MYSQL</h1>
+
+    <h2>Fromindex.html</h2>
+    <?php echo "Hellow" ?>
+</body>
